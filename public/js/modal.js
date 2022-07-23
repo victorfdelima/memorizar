66caf4255554ede786fdaaf2a4bf30ee88227279
Victor de Lima <vittinferreira@gmail.com>
2022-07-23 05:26:40 -0300

    Update modal.js

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,3 +1,20 @@
 $(window).on('load', function () {
+    localStorage.setItem("modal", true);
     $('#modalPadrao').modal('show');
+});
+
+$('.close').click(function (event) {
+    $('#modalPadrao').fadeOut();
+    event.preventDefault();
+});
+
+$(document).ready(function () {
+    var ls = localStorage.getItem("modal");
+    if (!ls) {
+        $('#modalPadrao').modal('show');
+    }
+})
+
+$('#modalPadrao').on('shown.bs.modal', function () {
+    localStorage.setItem("modal", false);
 });
