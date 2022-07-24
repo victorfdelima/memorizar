1b035fdfbfd5e9af1b6e307989a86e31e1ece468
Victor de Lima <vittinferreira@gmail.com>
2022-07-23 23:53:52 -0300

    Favicon Icon

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
-const path = require('path')
+const path = require('path');
+const favicon = require('serve-favicon');
 
 port = process.env.PORT
-
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.png')));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
